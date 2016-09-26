3cd3190079de1722e2718d3874a35f3bf3ec0ce4
zhanpe <1074937998@qq.com>
2016-09-26 11:09:35 +0800

    new
    we

---
+++ Myclass.lua
@@ -0,0 +1,32 @@
+function printf(fmt, ...)
+    print(string.format(tostring(fmt), ...))
+end
+function class(classname, super)
+    local superType = type(super)
+    local cls
+
+    if superType ~= "function" and superType ~= "table" then
+        superType = nil
+        super = nil
+    end
+        -- inherited from Lua Object
+        if super then
+            cls = {}
+            setmetatable(cls, {__index = super})
+            cls.super = super
+        else
+            cls = {ctor = function() end}
+        end
+
+        cls.__cname = classname
+        cls.__ctype = 2 -- lua
+        cls.__index = cls
+
+        function cls.new(...)
+            local instance = setmetatable({}, cls)
+            instance.class = cls
+            instance:ctor(...)
+            return instance
+        end
+    return cls
+end
